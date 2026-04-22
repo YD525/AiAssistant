1781bacf5cb6f4ec72bf6fc78ef69da1fa889e7b
YD525 <525084464@qq.com>
2026-04-22 20:12:16 +0800

    UPDate

---
+++ AiAssistant/AI/AICenter.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace AiAssistant.AI
+{
+    public class AISetting
+    {
+        public bool EnableChatGpt { get; set; } = false;
+        public bool EnableGemini { get; set; } = false;
+        public bool EnableLMStudio { get; set; } = false;
+
+        public string ChatGptKey { get; set; } = "";
+
+        public string GeminiKey { get; set; } = "";
+
+        public int LMStudioPort { get; set; } = 1234;
+
+    }
+    public class AICenter
+    {
+        public AISetting LocalSetting = new AISetting();
+
+
+        private static readonly byte[] XorKey = Encoding.UTF8.GetBytes("AiAssistant");
+        private static byte[] XOREncrypt(byte[] data)
+        {
+            byte[] result = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = (byte)(data[i] ^ XorKey[i % XorKey.Length]);
+            }
+            return result;
+        }
+
+        private static byte[] XORDecrypt(byte[] data)
+        {
+            return XOREncrypt(data);
+        }
+
+
+    }
+}
